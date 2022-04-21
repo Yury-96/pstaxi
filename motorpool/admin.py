6091148062fa7f69743c59c47d7176f4efbeedd7
Yury <fiz12@bk.ru>
2022-04-21 15:26:23 +0300

    Initial commit

---
+++ motorpool/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+
+@admin.register(models.Brand)
+class BrandAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
---
+++ motorpool/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=104, verbose_name='Название')
+
+    class Meta:
+        verbose_name_plural = 'Бренды'
+        verbose_name = 'Бренд'
+
+    class Brand(models.Model):
+        title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
